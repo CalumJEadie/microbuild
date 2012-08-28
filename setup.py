f595b383c173ae0fa359f5063b35832badd407f9
Calum J. Eadie <calum@calumjeadie.com>
2012-08-28 22:56:35 +0100

    Preparing for distribution.

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
    version="0.1.0",
    author="Calum J. Eadie",
    author_email="calum@calumjeadie.com",
+   url="https://github.com/CalumJEadie/microbuild",
    packages=["microbuild"],
-   license=open("LICENSE.txt").read(),
-   description="Really simple Python build tool.",
+   license="MIT License",
+   description="Lightweight Python Build Tool.",
    long_description=open("README.md").read(),
    install_requires=[],
 )
