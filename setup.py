7253113984d43b3298e6394635d172b7f7182978
Calum J. Eadie <calum@calumjeadie.com>
2012-08-29 22:06:03 +0100

    Marking new minor version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
    name="microbuild",
-   version="0.1.0",
+   version="0.2.0",
    author="Calum J. Eadie",
    author_email="calum@calumjeadie.com",
    url="https://github.com/CalumJEadie/microbuild",
